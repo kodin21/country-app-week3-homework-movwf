9bfb6f9ebc8dc9b1a4266090aa49a6ae31d4967e
movwf <movwf@protonmail.com>
2021-07-21 20:56:13 +0300

    UI: Header component created.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
