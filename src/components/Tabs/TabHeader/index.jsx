93771b185360e8f081c348781ca1d51b9198dc2f
movwf <movwf@protonmail.com>
2021-07-21 21:09:42 +0300

    UI: Tab header buttons established into header.

--- src/components/Tabs/TabHeader/index.jsx
+++ src/components/Tabs/TabHeader/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TabButton from './TabButton';
 
 function TabHeader({ setCurrentTab }) {
   return (
     <div className="h-12 w-full flex flex-row border-b-2">
-      TabButton  
+      <TabButton {...{ setCurrentTab, tabName: 'Country List', tabSelector: 'country-list' }} />
+      <TabButton {...{ setCurrentTab, tabName: 'Statistics', tabSelector: 'statistics' }} />
     </div>
   );
 }
